501afa1207b9997ec7cf9c2500e97f7111ebf371
Prashant Singh <octanesingh@gmail.com>
2022-10-06 14:55:11 +0100

    Converting the various elements to tsx

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,9 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 
+type SlideProps = {
+    slide: string;
+}
 
-function Slide(props:{slide:string}) {
-    const { slide } = props;
+const Slide:React.FunctionComponent<SlideProps> = (props) => {
+ 
+    let { slide } = props;
     const slideCss = css`width:100%; height:100vh; background-image:url(${slide}); background-size:cover; width:3072px; background-repeat: no-repeat;
     background-position: center`;
     return (
--- src/components/SliderContent.tsx
+++ src/components/SliderContent.tsx
@@ -1,8 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import {css} from '@emotion/react'
-import Slide from './Slide.tsx'
+import Slide from './Slide'
+import React from "react";
 
-function SliderContent(props) {
+type SliderContentProps = {
+  width: number;
+  translate: number;
+  transition:number;
+  children: React.ReactElement[];
+}
+
+const SliderContent:React.FunctionComponent<SliderContentProps> = (props) =>{
     const {width,translate} = props;
     const SliderContentCss = css`width:${props.width*3}px; height:100vh; overflow:hidden; display:flex;transition:${props.transition}s;transform:translateX(${translate}px)`;
   return (
